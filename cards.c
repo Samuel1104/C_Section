0e244b6c8c65d20a4f36b9f9efe6ee656e7c82d4
Samuel1104 <sam.chinedum@gmail.com>
2023-09-21 15:13:01 +0100

    For_cards: Re-downloaded from github after change of name of cards to card_counter after last commit. For cards_counter: Change of name; but fixed code to stop infinite looping.

--- cards.c
+++ cards.c
@@ -2,7 +2,7 @@
  * Program to evaluate face values.
  * Released under the Vegas Public License.
  * (c) 2014 The College Blackjack Team.
- * pg 37 (Head First C)
+ * pg 28 (Head First C)
  */
 
 #include <stdio.h>
@@ -10,35 +10,20 @@
 int main()
 {
 	char card_name[3];
-	int count = 0;
-	while (card_name) {
-		puts("Enter the card name: ");
-		scanf("%2s", card_name);
-		int val = 0;
-		switch(card_name[0]) {
-		case 'K':
-		case 'Q':
-		case 'J':
-			val = 10;
-			break;
-		case 'A':
-			val = 11;
-			break;
-		case 'X':
-			continue;
-		default:
-			val = atoi(card_name);
-			if ((val < 1) || (val > 10)) {
-				puts("I don't understand that value!");
-				continue;
-			}
-		}
-		if ((val > 2) && (val < 7)) {
-			count++;
-		} else if (val == 10) {
-			count--;
-		}
-		printf("Current count: %i\n", count);
+	puts("Enter the card_name: ");
+	scanf("%2s", card_name);
+	int val = 0;
+	switch(card_name[0]) {
+	case 'K':
+	case 'Q':
+	case 'J':
+		val = 10;
+		break;
+	case 'A':
+		val = 11;
+		break;
+	default:
+		val = atoi(card_name);
 	}
 	return 0;
 }
