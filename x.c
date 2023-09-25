04ff39097d914859363ec8c6a77e868b001ba7ee
Samuel1104 <sam.chinedum@gmail.com>
2023-09-25 19:11:32 +0100

    Pointer Address

--- x.c
+++ x.c
@@ -1,4 +1,6 @@
-/* To try how to find where a variable is stored in memory using the & operator. */
+/* To try how to find where a variable is stored in memory using the & operator.
+ * To store the address with a pointer variable.
+ */
 
 #include <stdio.h>
 
@@ -6,5 +8,8 @@ int main()
 {
 	int x = 4;
 	printf("x lives at %p\n", &x);
-return 0;
+
+	int *address_of_x = &x;
+	printf("%d\n", *address_of_x);
+	return 0;
 }
