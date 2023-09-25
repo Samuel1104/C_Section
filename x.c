2a3b68b4ad8a8b59e6694d26d86156ad6823d323
Samuel1104 <sam.chinedum@gmail.com>
2023-09-25 15:48:09 +0100

    Get the address of a variable

---
+++ x.c
@@ -0,0 +1,10 @@
+/* To try how to find where a variable is stored in memory using the & operator. */
+
+#include <stdio.h>
+
+int main()
+{
+	int x = 4;
+	printf("x lives at %p\n", &x);
+return 0;
+}
